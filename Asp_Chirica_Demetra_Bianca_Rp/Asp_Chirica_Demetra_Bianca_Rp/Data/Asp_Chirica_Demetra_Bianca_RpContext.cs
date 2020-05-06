4552476c15844ad98f14e5bd5b04a29db2555462
BiancaChirica <48147775+BiancaChirica@users.noreply.github.com>
2020-05-06 15:32:40 +0300

    laborator 9

---
+++ Asp_Chirica_Demetra_Bianca_Rp/Asp_Chirica_Demetra_Bianca_Rp/Data/Asp_Chirica_Demetra_Bianca_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Chirica_Demetra_Bianca_Rp.Models;
+
+namespace Asp_Chirica_Demetra_Bianca_Rp.Data
+{
+    public class Asp_Chirica_Demetra_Bianca_RpContext : DbContext
+    {
+        public Asp_Chirica_Demetra_Bianca_RpContext (DbContextOptions<Asp_Chirica_Demetra_Bianca_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Chirica_Demetra_Bianca_Rp.Models.Movie> Movie { get; set; }
+    }
+}
